fc5ecf86b234543e70e2a565c511cfc2312e17ae
edougher <ejdougher@gmail.com>
2021-03-06 21:37:39 -0600

    commit tree bf8daf200c00f72f35baa1fd26445a82ef927326

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,16 +1,20 @@
 export default function shoppingListItemReducer(
 	state = {
 		items: []
-	},
-	action
-) {
+	  },
+	  action
+	){
+	console.log(action);
 	switch (action.type) {
 		case 'INCREASE_COUNT':
+			console.log('Current state.items lengths', state.items.length);
+			console.log('Updating stae.items length', state.items.length + 1);
 			return {
 				...state,
 				items: state.items.concat(state.items.length + 1)
 			}
 		default:
+			console.log('Initial state.items length' , state.items.length);
 			return state;
-	}
+}
 }
